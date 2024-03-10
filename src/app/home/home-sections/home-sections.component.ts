85dd804acb3dee003bf9d30665a9e2f69bde8e30
momen-alshouha <momenalshouha@outlook.com>
2024-03-10 20:06:53 +0300

    home sections component and section component generated , home images added

---
+++ src/app/home/home-sections/home-sections.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-sections',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-sections.component.html',
+  styleUrl: './home-sections.component.scss'
+})
+export class HomeSectionsComponent {
+
+}
